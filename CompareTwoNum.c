2b9858ebc70ddd26fba5ec0977a3b5010aef842a
KimSeongYeon <syg01103@naver.com>
2022-06-05 18:08:57 +0900

    두 숫자를 비교하여 문자 훌력

---
+++ CompareTwoNum.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <string.h>
+
+int main()
+{
+  int num = 3;
+  int A[3], B[3];
+
+  for (int i = 0; i < num; i++)
+  {
+    scanf("%d %d", &A[i], &B[i]);
+  }
+
+  for (int i = 0; i < num; i++)
+  {
+    printf("#%d ", i + 1);
+
+    if (A[i] > B[i])
+      printf("A");
+
+    else if (A[i] < B[i])
+      printf("B");
+
+    else if (A[i] == B[i])
+    {
+      printf("D");
+    }
+
+    printf("\n");
+  }
+
+  return 0;
+}
